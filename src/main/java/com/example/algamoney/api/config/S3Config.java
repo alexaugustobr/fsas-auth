01f58dbdc1147d274385651721bb276e9a355a37
Alex <alex.silva@algaworks.com.br>
2021-05-14 09:40:14 -0300

    FSAS Atualizado

---
+++ src/main/java/com/example/algamoney/api/config/S3Config.java
@@ -0,0 +1,46 @@
+package com.example.algamoney.api.config;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+	
+	@Autowired
+	private AlgamoneyApiProperty property;
+	
+//	@Bean
+	public AmazonS3 amazonS3() {
+//		AWSCredentials credenciais = new BasicAWSCredentials(
+//				property.getS3().getAccessKeyId(), property.getS3().getSecretAccessKey());
+//
+//		AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
+//				.withCredentials(new AWSStaticCredentialsProvider(credenciais))
+//				.withRegion(Regions.US_EAST_1)
+//				.build();
+//
+//		if (!amazonS3.doesBucketExistV2(property.getS3().getBucket())) {
+//			amazonS3.createBucket(
+//					new CreateBucketRequest(property.getS3().getBucket()));
+//
+//			BucketLifecycleConfiguration.Rule regraExpiracao =
+//					new BucketLifecycleConfiguration.Rule()
+//					.withId("Regra de expiração de arquivos temporários")
+//					.withFilter(new LifecycleFilter(
+//							new LifecycleTagPredicate(new Tag("expirar", "true"))))
+//					.withExpirationInDays(1)
+//					.withStatus(BucketLifecycleConfiguration.ENABLED);
+//
+//			BucketLifecycleConfiguration configuration = new BucketLifecycleConfiguration()
+//					.withRules(regraExpiracao);
+//
+//			amazonS3.setBucketLifecycleConfiguration(property.getS3().getBucket(),
+//					configuration);
+//		}
+//
+//		return amazonS3;
+		return null;
+	}
+}
